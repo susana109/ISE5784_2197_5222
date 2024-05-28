701fcc9ed134065c323857963b9310d18d2e36b2
bityasusana <125869881+bityasusana@users.noreply.github.com>
2024-05-28 14:37:05 +0300

    stage 1 project

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,34 @@
+package primitives;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @org.junit.jupiter.api.Test
+    void testTestEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestHashCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDistanceSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSubtract() {
+    }
+}
