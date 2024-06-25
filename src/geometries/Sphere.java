9c2b878a3a086f6fc2325a507bbc068494cfe202
bityasusana <125869881+bityasusana@users.noreply.github.com>
2024-06-25 11:35:45 +0300

    fin etape 3

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -28,6 +28,7 @@ public class Sphere extends RadialGeometry {
      * @param point the point on the sphere
      * @return the normal vector at the given point
      */
+
     public Vector getNormal(Point point) {
         // Compute the vector from the center of the sphere to the point
         Vector normal = point.subtract(center);
@@ -39,39 +40,51 @@ public class Sphere extends RadialGeometry {
     /**
      * calculates the points of the intersections with the given ray to the sphere
      * @param ray Ray which should intersect with the sphere
-     * @return List Point3D which should return null on none point, or list of points that intersect the sphere
+     * @return List Point which should return null on none point, or list of points that intersect the sphere
      */
+
     @Override
     public List<Point> findIntersections(Ray ray) {
         Point p0 = ray.getHead();
         Vector v = ray.getDirection();
         Vector u;
-        try {
-            //calculates the vector from the head of the ray to the center of the sphere
-            u = (this.center).subtract(p0);
-            //if p0 is equal to the center of the sphere, it throws an exception
-            //it returns only one intersection point (radius distance from p0)
-        } catch (IllegalArgumentException e) {
+
+
+        // Calculates the vector from the head of the ray to the center of the sphere
+        u = this.center.subtract(p0);
+
+        // Check if p0 is equal to the center of the sphere
+        if (this.center.equals(p0)) {
+            // p0 is equal to the center of the sphere, return the intersection point
             return List.of(ray.getPoint(this.radius));
         }
+
+// Continue with the rest of your code...
+
         //calculates tm the projection of u on v (alignZero is used for accuracy)
         double tm = alignZero(v.dotProduct(u));
+
         //calculates the distance squared of p0 from the center of the sphere
         //if tm=0, v is orthogonal to u
         //if not, calculates dsquared with the Pythagore's theorem
         double dSquared = (tm == 0) ? u.lengthSquared() : u.lengthSquared() - tm * tm;
+
         //calculates the squared distance from tm to the sphere
         double thSquared = alignZero(this.radius *this.radius - dSquared);
+
         //if it's negative , there is no intersection point
         if (thSquared <= 0) return null;
 
         double th = alignZero(Math.sqrt(thSquared));//distances to the intersection points
         if (th == 0) return null;// the ray is tangent to the sphere
+
         //distances of intersection points on the ray
         double t1 = alignZero(tm - th);
         double t2 = alignZero(tm + th);
+
         //if the points are before the head of the ray there is no intersection points
         if (t1 <= 0 && t2 <= 0) return null;
+
         //if the points are after the head of the ray it returns the points
         if (t1 > 0 && t2 > 0) return List.of(ray.getPoint(t1), ray.getPoint(t2)); //P1 , P2
         if (t1 > 0)
--- unittests/geometries/Sphere.java
+++
@@ -1,4 +0,0 @@
-package geometries;
-
-public class Sphere {
-}
